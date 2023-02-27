fab278dc736b00d7c3ed3b1bee8e108899682fd4
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2023-02-27 19:14:38 +0200

    adds pokemonDetails component

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,3 +1,41 @@
-export function PokemonDetails() {
-  return <div>Details</div>;
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+
+export function PokemonDetails({ pokemonInfo }) {
+  const { sprites, types, name, abilities } = pokemonInfo;
+
+  function getAbilityNames() {
+    return abilities.map(({ ability }) => ability.name).join(', ');
+  }
+
+  function getTypes() {
+    return types.map(({ type }) => type.name).join(', ');
+  }
+
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+      <CardMedia
+        component="img"
+        alt={`${name}'s image from the back`}
+        width="100%"
+        image={sprites.back_default}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h3" component="h2">
+          {name}
+        </Typography>
+        <Typography variant="h6" color="text.primary">
+          Abilities:{' '}
+          <Typography variant="secondary" color="text.secondary">
+            {getAbilityNames()}
+          </Typography>
+        </Typography>
+        <Typography variant="h6" color="text.primary">
+          Types:{' '}
+          <Typography variant="secondary" color="text.secondary">
+            {getTypes()}
+          </Typography>
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from 'react';
 import { Card, CardContent, Typography, Grid, Button } from '@mui/material';
 import axios from 'axios';
+import { PokemonDetails } from 'components/PokemonDetails/PokemonDetails';
 
 axios.defaults.baseURL = 'https://pokeapi.co/api/v2/';
 
 export function PokemonList() {
-  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonsData, setPokemonsData] = useState([]);
+  const [pokemonInfo, setPokemonInfo] = useState(null);
   const [nextLink, setNextLink] = useState(null);
   const [prevLink, setPrevLink] = useState(null);
 
   function fetchPokemons(url) {
     axios(url || 'pokemon?limit=12').then(({ data }) => {
-      setPokemonData(data.results);
+      setPokemonsData(data.results);
       setNextLink(data.next);
       setPrevLink(data.previous);
+    });
+  }
+
+  function getPokemonInfo(url) {
+    axios(url).then(({ data }) => {
+      setPokemonInfo(data);
     });
   }
 
@@ -21,31 +29,47 @@ export function PokemonList() {
     fetchPokemons();
   }, []);
 
-  console.log(nextLink, prevLink);
-
   return (
-    <div style={{ width: 700 }}>
-      <Grid container spacing={3}>
-        {pokemonData.map(pokemon => (
-          <Grid item xs={12} sm={6} md={4} key={pokemon.url}>
-            <Card>
-              <CardContent>
-                <Typography variant="h4" component="h2">
-                  {pokemon.name}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 30 }}>
-        <Button disabled={!prevLink} onClick={() => fetchPokemons(prevLink)}>
-          Prev
-        </Button>
-        <Button disabled={!nextLink} onClick={() => fetchPokemons(nextLink)}>
-          Next
-        </Button>
+    <div style={{ display: 'flex' }}>
+      <div style={{ width: 500, marginRight: 50 }}>
+        <Grid container spacing={3}>
+          {pokemonsData.map(pokemon => (
+            <Grid item xs={12} sm={6} md={4} key={pokemon.url}>
+              <Card onClick={() => getPokemonInfo(pokemon.url)}>
+                <CardContent>
+                  <Typography variant="h4" component="h2">
+                    {pokemon.name}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <div
+          style={{ display: 'flex', justifyContent: 'center', marginTop: 30 }}
+        >
+          <Button disabled={!prevLink} onClick={() => fetchPokemons(prevLink)}>
+            Prev
+          </Button>
+          <Button disabled={!nextLink} onClick={() => fetchPokemons(nextLink)}>
+            Next
+          </Button>
+        </div>
       </div>
+
+      {pokemonInfo ? (
+        <PokemonDetails pokemonInfo={pokemonInfo} />
+      ) : (
+        <h2
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          Pick a pokemon to see details
+        </h2>
+      )}
     </div>
   );
 }
